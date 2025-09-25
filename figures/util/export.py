44ec919c27e87ee29173fea6f3d40f2df7db40e8
m-guggenmos <monaden@gmail.com>
2025-09-25 14:34:27 +0200

    Replace symbolic links with files

--- figures/util/export.py
+++ figures/util/export.py
@@ -1,1 +1,6 @@
-../../../coco_plot/export.py
+import matplotlib.pyplot as plt
+import os
+
+def savefig(path, bbox_inches='tight', pad_inches=0.012, dpi=300, **kwargs):
+    print(f'Figure saved to {os.path.abspath(path)}')
+    plt.savefig(path, bbox_inches=bbox_inches, pad_inches=pad_inches, dpi=dpi, **kwargs)
--- figures/util/fontsize.py
+++ figures/util/fontsize.py
@@ -1,1 +1,38 @@
-../../../coco_plot/fontsize.py
+import matplotlib.pyplot as plt
+
+def set_fontsize(label=None, xlabel=None, ylabel=None, tick=None, xtick=None, ytick=None, title=None, set=None):
+
+    fig = plt.gcf()
+
+    if set == 'default':
+        label = 14
+        tick = 12
+        title = 16
+
+    for ax in fig.axes:
+        if xlabel is not None:
+            ax.xaxis.label.set_size(xlabel)
+        elif label is not None:
+            ax.xaxis.label.set_size(label)
+        if ylabel is not None:
+            ax.yaxis.label.set_size(ylabel)
+        elif label is not None:
+            ax.yaxis.label.set_size(label)
+
+        if xtick is not None:
+            for ticklabel in (ax.get_xticklabels()):
+                ticklabel.set_fontsize(xtick)
+        elif tick is not None:
+            for ticklabel in (ax.get_xticklabels()):
+                ticklabel.set_fontsize(tick)
+        if ytick is not None:
+            for ticklabel in (ax.get_yticklabels()):
+                ticklabel.set_fontsize(ytick)
+        elif tick is not None:
+            for ticklabel in (ax.get_yticklabels()):
+                ticklabel.set_fontsize(tick)
+
+        if title is not None:
+            ax.title.set_fontsize(title)
+
+
