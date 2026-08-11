96a140a90e23eee1c1b765ed01a91c0930af87d3
m-guggenmos <monaden@gmail.com>
2026-08-11 22:27:02 +0200

    Minor formatting changes

--- figures/util/regression.py
+++ figures/util/regression.py
@@ -1,16 +1,17 @@
-import os
 import dataframe_image as dfimg
-from coco_hothand.figures.util.stack_images import stack_images
-from coco_hothand.figures.config import predictor_map
-from coco_hothand.util.stats_util.print_ranefs import print_ranefs
-import pandas as pd
 import numpy as np
+import os
+import pandas as pd
 import rpy2
 from PIL import Image
 from itertools import combinations
 
+from coldhand.figures.config import predictor_map
+
+
 def get_standardizer(df, dv, predictors):
     """
+    Compute standardizer to compute standardized regression coefficients
 
     Args:
         df (pd.DataFrame): data
@@ -39,14 +40,16 @@ def get_standardizer(df, dv, predictors):
     return standardizer
 
 
-def format_regression_table(result, predictor_map=None, standardizer=None, reorder_predictors=None):
+def format_regression_table(result, predictor_map=None, standardizer=None, reorder_predictors=None, exact_p=False):
     """
+    Format regression table
 
     Args:
         result (pd.DataFrame): unformated Pandas dataframe containing the regression table
         predictor_map (dict[str, str], optional): renaming dictionary for the predictors
         standardizer (dict[str, float], optional): standardizer dictionary for the predictors
         reorder_predictors (dict[int, int], optional): position reorder dictionary for the predictors
+        exact_p (bool, optional): set True to get p-values in scientific notation
 
     Returns:
         result_fm (pd.DataFrame): formated Pandas dataframe containing the regression table
@@ -82,7 +85,10 @@ def format_regression_table(result, predictor_map=None, standardizer=None, reord
 
     result_fmt = result_fmt[[''] + list(cols.keys())].rename(columns=cols)
 
-    result_fmt.p = ['< .001' if p < 0.001 else f'{p:.3f}'[1:] for p in result_fmt.p]
+    if exact_p:
+        result_fmt.p = [f'{p:.1e}' if p < 0.001 else f'{p:.3f}' for p in result_fmt.p]
+    else:
+        result_fmt.p = ['< .001' if p < 0.001 else f'{p:.3f}'[1:] for p in result_fmt.p]
 
 
     if reorder_predictors is not None:
@@ -96,51 +102,54 @@ def format_regression_table(result, predictor_map=None, standardizer=None, reord
 
     return result_fmt
 
-def export_regression_table(result, filename, ci95_as_percent=False, df=None, reorder_predictors=None, standardizer=None):
+def export_regression_table(result, filename, omit_intercept_stats=True, ci95_as_percent=False, reorder_predictors=None, standardizer=None, print_google_doc_size=False, exact_p=False):
 
-    rdf = print_ranefs(result)
     if ci95_as_percent:
         print(f"\nConfidence interval for the difference:\nCI95 = [{100*result.iloc[1]['2.5_ci']:.3f}%; {100*result.iloc[1]['97.5_ci']:.3f}%]")
-        print(f"\nConfidence interval for the difference:\nCI95 = [{result.iloc[1]['2.5_ci']:.4f}; {result.iloc[1]['97.5_ci']:.4f}] {result.iloc[1]['Estimate']:.4f} ± {np.abs(result.iloc[1]['Estimate'] - result.iloc[1]['97.5_ci']):.4f}")
+        print(f"\nConfidence interval for the difference:\nCI95 = [{result.iloc[1]['2.5_ci']:.4f}; {result.iloc[1]['97.5_ci']:.4f}] "
+              f"{result.iloc[1]['Estimate']:.4f} ± {np.abs(result.iloc[1]['Estimate'] - result.iloc[1]['97.5_ci']):.4f}")
         print(f"CI95(odds) = [{100*(np.exp(result.iloc[1]['2.5_ci'])-1):.3f}%; {100*(np.exp(result.iloc[1]['97.5_ci'])-1):.3f}%]")
-        # pred_cat = set([k.split(')')[0][2:] for k in result.Estimate.index if k.startswith('C(') and 'prev_cond' not in k])
-        # p1 = odds1 / (1 + odds1)
     else:
         if result.index[1].startswith('C(prev_cond)'):
-            print(f"\nConfidence interval post-loss versus post-fold:\nCI95 = [{result.iloc[1]['2.5_ci']:.4f}; {result.iloc[1]['97.5_ci']:.4f}] {result.iloc[1]['Estimate']:.4f} ± {np.abs(result.iloc[1]['Estimate'] - result.iloc[1]['97.5_ci']):.4f}")
-            print(f"\nConfidence interval post-win versus post-fold:\nCI95 = [{result.iloc[2]['2.5_ci']:.4f}; {result.iloc[2]['97.5_ci']:.4f}] {result.iloc[2]['Estimate']:.4f} ± {np.abs(result.iloc[2]['Estimate'] - result.iloc[2]['97.5_ci']):.4f}")
+            print(f"\nConfidence interval post-loss versus post-fold:\nCI95 = [{result.iloc[1]['2.5_ci']:.4f}; {result.iloc[1]['97.5_ci']:.4f}] "
+                  f"{result.iloc[1]['Estimate']:.4f} ± {np.abs(result.iloc[1]['Estimate'] - result.iloc[1]['97.5_ci']):.4f}")
+            print(f"\nConfidence interval post-win versus post-fold:\nCI95 = [{result.iloc[2]['2.5_ci']:.4f}; {result.iloc[2]['97.5_ci']:.4f}] "
+                  f"{result.iloc[2]['Estimate']:.4f} ± {np.abs(result.iloc[2]['Estimate'] - result.iloc[2]['97.5_ci']):.4f}")
         else:
-            print(f"\nConfidence interval for the difference:\nCI95 = [{result.iloc[1]['2.5_ci']:.4f}; {result.iloc[1]['97.5_ci']:.4f}] {result.iloc[1]['Estimate']:.4f} ± {np.abs(result.iloc[1]['Estimate'] - result.iloc[1]['97.5_ci']):.4f}")
+            print(f"\nConfidence interval for the difference:\nCI95 = [{result.iloc[1]['2.5_ci']:.4f}; {result.iloc[1]['97.5_ci']:.4f}] "
+                  f"{result.iloc[1]['Estimate']:.4f} ± {np.abs(result.iloc[1]['Estimate'] - result.iloc[1]['97.5_ci']):.4f}")
     print(f"\nModel evidence:\nAIC = {result.attrs['AIC']:.5f}")
 
-    # caption_html = """
-    #     <div style="font-size:14px; font-style: italic; width: 100%; font-weight: bold; background-color: #f5f5f5; padding: 5px">
-    #         Fixed effects
-    #     </div>
-    # """
-    # coefs_f = format_regression_table(result, predictor_map=predictor_map, replacements_extra={'game type': 'solo game type'})
-    coefs_f = format_regression_table(result, predictor_map=predictor_map, reorder_predictors=None, standardizer=standardizer)
+    coefs_f = format_regression_table(result, predictor_map=predictor_map,
+                                      reorder_predictors=reorder_predictors, standardizer=standardizer, exact_p=exact_p)
     print('\nRegression table:\n', coefs_f[coefs_f.columns[1:]])
-    # coefs_styler = coefs_f.style.hide(axis="index").format(dict(Estimate='{:.2f}', SE='{:.2f}', t='{:.2f}')).map(lambda x: 'font-weight: bold', subset=['']).set_caption(caption_html)
-    coefs_styler = coefs_f.style.hide(axis="index").format(dict(Estimate='{:.2f}', SE='{:.2f}', t='{:.2f}')).map(lambda x: 'font-weight: bold', subset=[''])
+    # if omit_intercept_stats:
+    #     for col in ('t', 'p', 'Sign.'):
+    #         coefs_f.at['(Intercept)', col] = ''
+    coefs_styler = coefs_f.style.hide(axis="index").format(
+        dict(Estimate='{:.3f}', b='{:.3f}', SE='{:.3f}', t='{:}' if omit_intercept_stats else '{:.1f}')).map(lambda x: 'font-weight: bold', subset=[''])
     path_fe = f"../figures/img/{filename.split('/')[-1].replace('.py', '.png')}"
-    # path_re = f"../figures/img/{filename.split('/')[-1].replace('.py', '_re.png')}"
     print(f'Figure saved to {os.path.abspath(path_fe)}')
+    if omit_intercept_stats:
+        coefs_styler.data['t'] = coefs_styler.data.apply(lambda x: f"{x['t']:.1f}", axis=1)
+        coefs_styler.data = coefs_styler.data.astype(dict(t=object))
+        for col in ('t', 'p', 'Sign.'):
+            coefs_styler.data.at['(Intercept)', col] = ''
     dfimg.export(coefs_styler, path_fe)
-    with Image.open(path_fe) as img:
-        print(f'Size: {img.width}px x {img.height}px = {2*img.width/100}cm x {2*img.height/100}cm')
-    # dfimg.export(rdf, path_re)
-    # stacked_img = stack_images(path_fe, path_re)
-    # stacked_img.save(f"../figures/img/{filename.split('/')[-1].replace('.py', '_stacked.png')}")
-    # os.remove(path_fe)
-    # os.remove(path_re)
+
+    # For Google doc only
+    if print_google_doc_size:
+        with Image.open(path_fe) as img:
+            print(f'Size: {img.width}px x {img.height}px = {2*img.width/100}cm x {2*img.height/100}cm')
 
 def prepare_result(model):
     result = model.coefs
-    # result.attrs['ranef'] = {'intercept': model.ranef['(Intercept)'].tolist(), var_ranS: model.ranef[f'{var_ranS}TRUE'].tolist()}
+    # result = model.params
     result.attrs['ranef'] = {k: model.ranef[k].tolist() for k in model.ranef.columns}
     result.attrs['ranef_var'] = {k: v for k, v in zip(model.ranef_var['Name'], model.ranef_var['Std']) if k != ''}
-    result.attrs['ranef_corr'] = {f'{iv1} versus {iv2}': corr for iv1, iv2, corr in zip(model.ranef_corr['IV1'], model.ranef_corr['IV2'], model.ranef_corr['Corr'])}
+    if model.ranef_corr is not None:
+        result.attrs['ranef_corr'] = {f'{iv1} versus {iv2}': corr for iv1, iv2, corr in
+                                      zip(model.ranef_corr['IV1'], model.ranef_corr['IV2'], model.ranef_corr['Corr'])}
     result.attrs['var_resid'] = np.var(model.residuals)
     for attr in ('AIC', 'BIC', 'logLike', 'formula'):
         result.attrs[attr] = getattr(model, attr).to_dict() if isinstance(getattr(model, attr), pd.DataFrame) else getattr(model, attr)
